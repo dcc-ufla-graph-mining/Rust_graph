dccc67fe98e44a890128f519db4b9eee646956e0
Daniel Messias Santos <danmes12345@gmail.com>
2023-12-07 08:19:34 -0300

    Tarjan ready and working. Returning articulations

--- graph_representation/src/tarjan.rs
+++ graph_representation/src/tarjan.rs
@@ -1,35 +1,42 @@
 use crate::representations::Graph;
 use Graph::Graph as Gr;
 
-fn DFS(graph: &Gr, discover_time: &mut Vec<isize>, low: &mut Vec<isize>, stack: &mut Vec<usize>, node: usize, mut counter: isize) {
-    for i in graph.get_adjacences(node) {
-        //println!("{}", i);
-        if low[i] == -1 {
-            discover_time[i] = counter;
-            low[i] = counter;
-            stack.push(i);
-            counter += 1;
+fn DFS(graph: &Gr, discover_time: &mut Vec<isize>, low: &mut Vec<isize>, node: usize, mut counter: isize, parent: isize, articulation: &mut Vec<usize>) {
+    discover_time[node] = counter;
+    low[node] = counter;
+    counter += 1;
+    let mut children = 0;
 
-            DFS(graph, discover_time, low, stack, i, counter);
+    for i in graph.get_adjacences(node) {
+        if low[i] == -1 {
+            children += 1;
+
+            DFS(graph, discover_time, low, i, counter, node as isize, articulation);
 
             if low[i] < low[node] { low[node] = low[i]; }
+            if parent != -1 && low[i] >= discover_time[node] {
+                articulation.push(node);
+            }
+
         }
+        else if i as isize != parent{
+            if low[node] > discover_time[i] { low[node] = discover_time[i]; }
+        }
+    }
+    if parent == -1 && children > 1 {
+        articulation.push(node);
     }
 }
 
-pub fn tarjan(graph: &Gr/*, large_graph: Gr*/) {
-    let mut discover_time: Vec<isize> = vec![-1; graph.get_num_nodes()];
-    let mut low: Vec<isize> = vec![-1; graph.get_num_nodes()];
-    let mut stack: Vec<usize> = Vec::new();
-    let global_counter = 1;
+pub fn tarjan(graph: &Gr) {
+    let num_nodes = graph.get_num_nodes();
+    let mut discover_time: Vec<isize> = vec![-1; num_nodes];
+    let mut low: Vec<isize> = vec![-1; num_nodes];
+    let mut articulation: Vec<usize> = Vec::new();
+    let global_counter = 0;
 
     discover_time[0] = 0;
     low[0] = 0;
-    DFS(&graph, &mut discover_time, &mut low, &mut stack, 0, global_counter);
-    println!("{:?}\n{:?}", discover_time, low);
-    for i in 0..graph.get_num_nodes() {
-        for j in graph.get_adjacences(i) {
-            if low[j] >= discover_time[i] { println!("opa"); }
-        }
-    }
+    DFS(&graph, &mut discover_time, &mut low, 0, global_counter, -1, &mut articulation);
+    println!("{:?}", articulation);
 }
